7398ba314dd35a85c3e4040bc41519b7ac6d4c24
youvegotnigel <nigeldave19@gmail.com>
2025-02-20 23:21:00 +0530

    Update PrintAsPDFTest.java

--- src/test/java/com/vitalhub/automation/demo/PrintAsPDFTest.java
+++ src/test/java/com/vitalhub/automation/demo/PrintAsPDFTest.java
@@ -5,12 +5,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.DownloadOptions;
 import com.codeborne.selenide.FileDownloadMode;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
 
 import static com.codeborne.pdftest.PDF.containsText;
 import static com.codeborne.selenide.Condition.clickable;
@@ -18,9 +20,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+// NOTE: GitHub issue - https://github.com/selenide/selenide/issues/2152
 public class PrintAsPDFTest {
 
     private static final By PRINT_CV_BUTTON = By.id("button-print-cv");
+    private static final String FILE_DOWNLOAD_PATH = System.getProperty("user.dir") + "/downloads/";
 
     @BeforeMethod
     public void setUp() {
@@ -29,8 +33,38 @@ public class PrintAsPDFTest {
         Configuration.browserSize = "1440x900";
         Configuration.browser = "chrome";
         Configuration.fileDownload = FileDownloadMode.FOLDER;
-        Configuration.downloadsFolder = System.getProperty("user.dir") + "/downloads/";
+        Configuration.downloadsFolder = FILE_DOWNLOAD_PATH;
         Configuration.reopenBrowserOnFail = false;
+
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> prefs = new HashMap<>();
+
+        prefs.put("download.prompt_for_download", false);
+        prefs.put("download.default_directory", FILE_DOWNLOAD_PATH);
+        prefs.put("savefile.default_directory", FILE_DOWNLOAD_PATH);
+        prefs.put("download.directory_upgrade", true);
+        prefs.put("download.extensions_to_open", "pdf");
+        prefs.put("safebrowsing.enabled", true);
+
+        // disable save password pop up on browser
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        // using --kiosk-printing to enable "silent printing"
+        options.addArguments("--kiosk");
+        options.addArguments("--kiosk-printing");
+        options.addArguments("--safebrowsing-disable-download-protection");
+        options.addArguments("--safebrowsing-disable-extension-blacklist");
+
+        // unknown error: DevToolsActivePort file doesn't exist
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+
+        // setting prefs to chrome options
+        options.setExperimentalOption("prefs", prefs);
+
+        // set driver with updated chrome options
+        Configuration.browserCapabilities = options;
 
         open(filePath);
         $x("//h1").shouldHave(text("Page with print"));
