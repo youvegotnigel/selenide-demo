ac576f0444cc3898db44881859b01f8ec3e67721
Nigel <nigeldave19@gmail.com>
2026-03-27 23:38:17 +0530

    Update SmallTest.java

--- src/test/java/com/vitalhub/knowledge/group/SmallTest.java
+++ src/test/java/com/vitalhub/knowledge/group/SmallTest.java
@@ -1,8 +1,10 @@
 package com.vitalhub.knowledge.group;
 
+import com.codeborne.selenide.WebDriverRunner;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
@@ -15,10 +17,15 @@ public class SmallTest {
     void is_waits_and_returns_false() {
         open("https://selenide.org/2024/02/07/selenide-7.1.0/#if-with-timeout");
 
+        //WebDriverRunner.getWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        System.out.println("currentTimeMillis: " + System.currentTimeMillis());
+        System.out.println(LocalDateTime.now());
         boolean isDisplayed = $("#nope").is(exist, Duration.ofSeconds(9));
-        assertThat(isDisplayed)
-                .as("Expect element #nope to not exist")
-                .isFalse();
+        System.out.println("currentTimeMillis: " + System.currentTimeMillis());
+        System.out.println(LocalDateTime.now());
+
+        assertThat(isDisplayed).as("Expect element #nope to not exist").isFalse();
 
         $$("#force-click").shouldHave(sizeGreaterThanOrEqual(2), Duration.ofSeconds(20));
     }
